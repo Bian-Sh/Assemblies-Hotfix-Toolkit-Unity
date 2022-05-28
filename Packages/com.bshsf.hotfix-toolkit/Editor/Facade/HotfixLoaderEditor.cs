f806ee8ad4fe18990106f13cd8228550e1d40204
边上海 <bshsf@qq.com>
2022-05-28 17:43:59 +0800

    HotfixLoader Inspector 面板绘制

---
+++ Packages/com.bshsf.hotfix-toolkit/Editor/Facade/HotfixLoaderEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEditor.AddressableAssets.GUI;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace zFramework.Hotfix.Toolkit
+{
+    [CustomEditor(typeof(HotfixLoader))]
+    public class HotfixLoaderEditor : Editor
+    {
+        SerializedProperty data;
+        SerializedObject so;
+        HotfixLoader loader;
+        public override void OnInspectorGUI()
+        {
+            this.serializedObject.Update();
+            if (null == data)
+            {
+                data = serializedObject.FindProperty("hotfixAssemblies");
+            }
+            if (!loader)
+            {
+                loader = target as HotfixLoader;
+            }
+
+            var rect = GUILayoutUtility.GetRect(GUIContent.none, EditorStyles.foldoutHeader);
+            var f_rect = new Rect(rect);
+            f_rect.width = EditorGUIUtility.labelWidth;
+            data.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(f_rect, data.isExpanded, "");
+            EditorGUI.EndFoldoutHeaderGroup();
+            EditorGUI.PropertyField(rect, data);
+
+            EditorGUI.indentLevel++;
+            var editorAsset = loader.hotfixAssemblies.editorAsset;
+            if (data.isExpanded && editorAsset)
+            {
+                if (null == so)
+                {
+                    so = new SerializedObject(editorAsset);
+                }
+                var asms = so.FindProperty("assemblies");
+                using (new GUILayout.VerticalScope(GUI.skin.box))
+                {
+                    for (int i = 0; i < asms.arraySize; i++)
+                    {
+                        var prop = asms.GetArrayElementAtIndex(i);
+                        using (new EditorGUI.DisabledScope(true))
+                        {
+                            EditorGUILayout.PropertyField(prop);
+                        }
+                        rect = GUILayoutUtility.GetLastRect();
+                        rect.x += EditorGUIUtility.labelWidth;
+                        rect.y += 1;
+                        rect.height -= 2;
+                        rect.width -= rect.x;
+                        if (Event.current.type == EventType.MouseDown)
+                        {
+                            if (rect.Contains(Event.current.mousePosition))
+                            {
+                                EditorGUIUtility.PingObject(editorAsset.assemblies[i].editorAsset);
+                                Event.current.Use();
+                            }
+                        }
+                    }
+                }
+            }
+            EditorGUI.indentLevel--;
+            EditorGUILayout.HelpBox("todo：自动处理被加载的程序集顺序问题", MessageType.Info);
+        }
+    }
+}
--- Packages/com.bshsf.hotfix-toolkit/Runtime/HotfixLoader.cs
+++ Packages/com.bshsf.hotfix-toolkit/Runtime/HotfixLoader.cs
@@ -9,28 +9,18 @@ namespace zFramework.Hotfix.Toolkit
     [DisallowMultipleComponent]
     public class HotfixLoader : MonoBehaviour
     {
-#if UNITY_EDITOR
-        [Header("加载热更逻辑：")]
-        [Tooltip("勾选则从 ab 中加载程序集！")]
-        public bool testLoad = false;
-#endif
         public AssetReferenceHotfixAssemblis hotfixAssemblies;
         IEnumerator Start()
         {
-#if UNITY_EDITOR
-            if (testLoad)
-#endif
+            //todo: 需要先加载依赖,按一定的顺序加载,希望全自动
+            var handler = hotfixAssemblies.LoadAssetAsync();
+            yield return handler;
+            if (handler.Status == AsyncOperationStatus.Succeeded)
             {
-                //todo: 需要先加载依赖,按一定的顺序加载,希望全自动
-                var handler = hotfixAssemblies.LoadAssetAsync();
-                yield return handler;
-                if (handler.Status == AsyncOperationStatus.Succeeded)
-                {
-                    var so = hotfixAssemblies.Asset as HotfixAssembliesData;
-                    yield return so.LoadAssemblyAsync();
-                }
-                hotfixAssemblies.ReleaseAsset();
+                var so = hotfixAssemblies.Asset as HotfixAssembliesData;
+                yield return so.LoadAssemblyAsync();
             }
+            hotfixAssemblies.ReleaseAsset();
         }
 
 #if UNITY_EDITOR
@@ -38,7 +28,7 @@ namespace zFramework.Hotfix.Toolkit
         {
             if (hotfixAssemblies == null)
             {
-                if (UnityEditor.AssetDatabase.TryGetGUIDAndLocalFileIdentifier(HotfixAssembliesData.Instance,out string guid,out long  id))
+                if (UnityEditor.AssetDatabase.TryGetGUIDAndLocalFileIdentifier(HotfixAssembliesData.Instance, out string guid, out long id))
                 {
                     hotfixAssemblies = new AssetReferenceHotfixAssemblis(guid);
                 }
