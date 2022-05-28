b8a12b9d56d2209dbd11dadd870a364255fdb193
边上海 <bshsf@qq.com>
2022-05-28 11:33:57 +0800

    解决了热更跑不通的问题了，同步更新 demo

--- Packages/com.bshsf.hotfix-toolkit/Samples~/Example/Scripts/Launcher/Init.cs
+++ Packages/com.bshsf.hotfix-toolkit/Samples~/Example/Scripts/Launcher/Init.cs
@@ -1,12 +1,11 @@
-﻿using UnityEngine;
-using UnityEngine.AddressableAssets;
+﻿using UnityEngine.AddressableAssets;
 
 namespace zFramework.Hotfix.Examples
 {
     /// <summary>
     /// 演示如何使用热更动态加载热更场景
     /// </summary>
-    public class Init : MonoBehaviour
+    public class Init 
     {
         [InitializeOnAssemblyLoad(0)]
         static void LoadScene() => Addressables.LoadSceneAsync("Hotfixed.unity");
