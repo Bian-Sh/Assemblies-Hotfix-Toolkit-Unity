b57533101712f5f998e1a8edddad9a8422f91a82
边上海 <bshsf@qq.com>
2022-06-03 10:20:51 +0800

    Update HotfixAssemblyInfoDrawer.cs
    取消 dragandrop ，没法实现我想要的拖拽中 show helpbox

--- Packages/com.bshsf.hotfix-toolkit/Editor/Drawer/HotfixAssemblyInfoDrawer.cs
+++ Packages/com.bshsf.hotfix-toolkit/Editor/Drawer/HotfixAssemblyInfoDrawer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditorInternal;
@@ -9,13 +10,16 @@ namespace zFramework.Hotfix.Toolkit
     [CustomPropertyDrawer(typeof(HotfixAssemblyInfo))]
     public class HotfixAssemblyInfoDrawer : PropertyDrawer
     {
-        GUIStyle style;
+        static Dictionary<string, DrawerState> drawerState = new Dictionary<string, DrawerState>();
         SimpleAssemblyInfo info = new SimpleAssemblyInfo();
-        string message;
+        internal class DrawerState
+        {
+            public GUIStyle style;
+            public float height;
+        }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-
             position.height = EditorGUIUtility.singleLineHeight + 2;
             var indent = EditorGUI.indentLevel;
             var labelWidth = EditorGUIUtility.labelWidth;
@@ -23,9 +27,21 @@ namespace zFramework.Hotfix.Toolkit
 
             #region Init Related  Propertys
             var assemblis = property.FindPropertyRelative("assembly");
+
+
+
             var bytesAsset = property.FindPropertyRelative("bytesAsset");
             var asm = assemblis.objectReferenceValue as AssemblyDefinitionAsset;
             var bts = bytesAsset.objectReferenceValue as TextAsset;
+            #endregion
+            #region Store Drawer State
+            var path = assemblis.propertyPath;
+            if (!drawerState.TryGetValue(path, out var data))
+            {
+                data = new DrawerState();
+                data.style = new GUIStyle(EditorStyles.foldout);
+                drawerState[path] = data;
+            }
             #endregion
 
             property.serializedObject.Update();
@@ -34,31 +50,26 @@ namespace zFramework.Hotfix.Toolkit
             #region Draw title
             string name = asm ? asm.name : "程序集未指定";
             var color = EditorStyles.foldout.normal.textColor;
-            if (null == style)
-            {
-                style = new GUIStyle(EditorStyles.foldout);
-            }
-            style.normal.textColor = asm ? color : Color.red;
+            data.style.normal.textColor = asm ? color : Color.red;
             if (!EditorGUIUtility.hierarchyMode)
             {
                 EditorGUI.indentLevel--;
             }
             EditorGUIUtility.labelWidth = position.width;
-            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, new GUIContent(name), style);
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, new GUIContent(name), data.style);
             EditorGUIUtility.labelWidth = labelWidth;
             if (!EditorGUIUtility.hierarchyMode)
             {
                 EditorGUI.indentLevel++;
             }
             #endregion
-
+            var orign = position;
             if (property.isExpanded)
             {
                 #region 绘制 程序集 定义文件字段
                 position.y += position.height + 6;
                 var field_rect = new Rect(position.x + EditorGUIUtility.labelWidth, position.y, position.width - EditorGUIUtility.labelWidth, position.height);
-                message = HandleDragAndDrop(field_rect, property);
-                Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}: after HandleDragAndDrop {message} ");
+                var message = string.Empty;
                 using (var check = new EditorGUI.ChangeCheckScope())
                 {
                     EditorGUI.PropertyField(position, assemblis);
@@ -67,11 +78,46 @@ namespace zFramework.Hotfix.Toolkit
                     {
                         property.serializedObject.ApplyModifiedProperties();
 
-                        #region 转存文件有效性验证：assembly 不为空且名称与 转存文件名称匹配
-                        asm = assemblis.objectReferenceValue as AssemblyDefinitionAsset;
-                        bts = bytesAsset.objectReferenceValue as TextAsset;
+                        #region Assembly 有效性校验
 
-                        Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}:  some changed to {asm?.name}");
+                        if (assemblis.objectReferenceValue is AssemblyDefinitionAsset asmdef &&asmdef)
+                        {
+                            #region 查重
+                            var ahm = property.serializedObject.targetObject as AssemblyHotfixManager;
+                            var isDuplicated = ahm.assemblies.Any(v => v.assembly && v.assembly.name == asmdef.name);
+                            #endregion
+                            if (!isDuplicated)
+                            {
+                                EditorJsonUtility.FromJsonOverwrite(asmdef.text, info);
+                                var isEditorAssembly = null != info.includePlatforms && info.includePlatforms.Length == 1 && info.includePlatforms[0] == "Editor";
+                                var isUsedByAssemblyCsharp = AssemblyHotfixManager.IsUsedByAssemblyCSharp(info.name);
+
+                                if (isEditorAssembly)
+                                {
+                                    message = $"编辑器程序集不可热更！ ";
+                                }
+                                if (isUsedByAssemblyCsharp)
+                                {
+                                    message = $"被 Assembly-CSharp 相关程序集引用不可热更！ ";
+                                }
+                            }
+                            else
+                            {
+                                message = $"已存在，无需重复添加！ ";
+                            }
+                        }
+                        else
+                        {
+                                message = $"请指定需要热更的程序集！ ";
+                        }
+                        #endregion
+
+
+
+                        #region 转存文件有效性验证：assembly 不为空且名称与 转存文件名称匹配
+                        property.serializedObject.Update();
+
+                        Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}:  some changed to {asm?.name} {message}");
 
                         if (bts && (!asm || !bts.name.Contains(asm.name)))
                         {
@@ -86,10 +132,9 @@ namespace zFramework.Hotfix.Toolkit
                 #endregion
                 #region Draw Message
                 Rect tip_rect = default;
-
                 if (!string.IsNullOrEmpty(message))
                 {
-                    Debug.Log($"Assembly Hotfix Toolkit: {message}");
+                    Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}: inside Draw Message {message}");
                     position.y += position.height + 4;
                     var height = EditorGUI.GetPropertyHeight(SerializedPropertyType.String, new GUIContent(message));
                     tip_rect = new Rect(position.x, position.y, position.width, height);
@@ -107,6 +152,7 @@ namespace zFramework.Hotfix.Toolkit
                 #endregion
 
             }
+            drawerState[path].height = position.y - orign.y + EditorGUIUtility.singleLineHeight + 2;
             EditorGUI.indentLevel = indent;
             EditorGUI.EndProperty();
         }
@@ -114,66 +160,8 @@ namespace zFramework.Hotfix.Toolkit
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             var assemblis = property.FindPropertyRelative("assembly");
-            var bytesAsset = property.FindPropertyRelative("bytesAsset");
-
-            var height = EditorGUIUtility.singleLineHeight + 6;
-            height += EditorGUI.GetPropertyHeight(assemblis) + 4;
-            height += EditorGUI.GetPropertyHeight(bytesAsset) + 4;
-
-            if (!string.IsNullOrEmpty(message))
-            {
-                Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}:  inside {message}");
-                height += EditorGUI.GetPropertyHeight(SerializedPropertyType.String, new GUIContent(message)) + 8;
-            }
-            return property.isExpanded ? height : base.GetPropertyHeight(property, label);
-        }
-
-        private string HandleDragAndDrop(Rect field_rect, SerializedProperty property)
-        {
-            bool isDragging = Event.current.type == EventType.DragUpdated && field_rect.Contains(Event.current.mousePosition);
-            bool isDropping = Event.current.type == EventType.DragPerform && field_rect.Contains(Event.current.mousePosition);
-            var rejectedDrag = true;
-            bool isEditorAssembly = false;
-            bool isUsedByAssemblyCsharp = false;
-            bool isDuplicated = false;
-            Debug.Log($"{nameof(HotfixAssemblyInfoDrawer)}: {property.FindPropertyRelative("assembly").objectReferenceValue?.name}  -  {isDragging} - {message}");
-            if (isDragging)
-            {
-                if (DragAndDrop.objectReferences[0] is AssemblyDefinitionAsset asmdef)
-                {
-                    #region 查重
-                    var ahm = property.serializedObject.targetObject as AssemblyHotfixManager;
-                    isDuplicated = ahm.assemblies.Any(v => v.assembly && v.assembly.name == asmdef.name);
-                    #endregion
-                    if (!isDuplicated)
-                    {
-                        EditorJsonUtility.FromJsonOverwrite(asmdef.text, info);
-                        isEditorAssembly = null != info.includePlatforms && info.includePlatforms.Length == 1 && info.includePlatforms[0] == "Editor";
-                        isUsedByAssemblyCsharp = AssemblyHotfixManager.IsUsedByAssemblyCSharp(info.name);
-
-                        if (isEditorAssembly)
-                        {
-                            message = $"编辑器程序集不可热更！ ";
-                        }
-                        if (isUsedByAssemblyCsharp)
-                        {
-                            message = $"被 Assembly-CSharp 相关程序集引用不可热更！ ";
-                        }
-                    }
-                    else
-                    {
-                        message = $"已存在，无需重复添加！ ";
-                    }
-                    rejectedDrag = isEditorAssembly || isUsedByAssemblyCsharp || isDuplicated;
-                    DragAndDrop.visualMode = rejectedDrag ? DragAndDropVisualMode.Rejected : DragAndDropVisualMode.Generic;
-                }
-            }
-            if (!rejectedDrag && isDropping)
-            {
-                property.objectReferenceValue = DragAndDrop.objectReferences[0];
-                Debug.LogError($"{nameof(HotfixAssemblyInfoDrawer)}:  asign  data");
-            }
-            return message;
+            drawerState.TryGetValue(assemblis.propertyPath, out var data);
+            return data?.height??base.GetPropertyHeight(property,label);
         }
 
         [Serializable]
